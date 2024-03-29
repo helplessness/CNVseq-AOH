72a22d3eb37e6497999f7a7c75c8d33ff8622aa7
helplessness <18200134298@163.com>
2024-03-29 14:52:46 +0800

    rewrite main

--- LP_WGS_hunter/__main__.py
+++ LP_WGS_hunter/__main__.py
@@ -15,7 +15,7 @@ import tempfile
 from LP_WGS_hunter import pre_loh
 
 LOG_FORMAT = '%(asctime)s-%(levelname)s-%(filename)s(%(lineno)s)-%(message)s'
-app = typer.Typer(help='PGTA异倍体分析')
+app = typer.Typer(help='Recurrent neural network for predicting absence of heterozygosity from low pass WGS with ultra-low depth')
 
 # Encodes the rows of the legend table
 leg_tuple = collections.namedtuple(
@@ -78,7 +78,7 @@ def one_file(output_origin_dir, origin_path, control_dir, file_class, debug, sam
                 run_from_bam.run_main_bam(file_name.as_posix(
                 ), output_dir, sample_name, control_dir, np=np,include_x=include_x,ref=ref)
             else:
-                print('确认文件是否为fq或bam')
+                print('Verify that the file is fq or bam')
         except Exception as e:
             logging.info(str(e))
             logging.info(sample_name+'\n')
@@ -87,18 +87,18 @@ def one_file(output_origin_dir, origin_path, control_dir, file_class, debug, sam
 
 
 @app.command()
-def run(input_path: str = typer.Argument(help='输入文件的绝对路径', default=None),
-        output_path: str = typer.Argument(help='输出文件夹的绝对路径', default=None),
+def run(input_path: str = typer.Argument(help='the absolute path to the input file', default=None),
+        output_path: str = typer.Argument(help='The absolute path to the output folder', default=None),
         input_class: str = typer.Option(
-        help='输入文件的类型是文件夹或者单个文件(one or mult)', default='one'),
+        help='The type of input file is a folder or a single file (one or mult).', default='one'),
         file_class: str = typer.Option(
-        help='输入文件的类型fq文件或bam文件(fq or bam)', default='fq'),
+        help='Type of input file fq file or bam file (fq or bam)', default='fq'),
         ref: str = typer.Option(
-        help='基因组版本号', default='hg19'),
-        debug: bool = typer.Option(help='输出debug信息', default=False),
-        np: int = typer.Option(help='使用CPU核数量', default=32),
-        sample_id: str = typer.Option(help='输出样本结果文件名，不指定使用fq/bam文件名（仅单文件分析模式适用）', default=None),
-        include_x:bool = typer.Option(help='是否输出X染色体信息', default=False),
+        help='Genome version number', default='hg19'),
+        debug: bool = typer.Option(help='Output debugging information', default=False),
+        np: int = typer.Option(help='Number of CPU cores used', default=32),
+        sample_id: str = typer.Option(help='Output sample result file name, fq/bam file name is not specified (single file analysis mode only applies)', default=None),
+        include_x:bool = typer.Option(help='Whether to output X chromosome information', default=False),
         ):
 
     control_dir = Path(config.CONREOL_DIR)
@@ -117,60 +117,16 @@ def run(input_path: str = typer.Argument(help='输入文件的绝对路径', def
     #     os.system(f'rm {output_origin_dir}/*/*.sam')
     #     os.system(f'rm {output_origin_dir}/*/*.bwa')
 
-@app.command()
-def run_file(
-    sample_info: str = typer.Option(..., help='样本信息表格, TAB分割，"sample_id fq-path"'),
-    result_dir: str = typer.Option(..., help='结果文件夹'),
-    ref: str = typer.Option(help='基因组版本号', default='hg19'),
-    debug: bool = typer.Option(help='输出debug信息', default=False),
-    include_x:bool = typer.Option(help='是否输出X染色体信息', default=False),
-):
-    sample_df = pd.read_csv(sample_info, sep='\t', names=['sample_id', 'fq_path'])
-    for idx, row in sample_df.iterrows():
-        print(f'processing sample ({idx}):', row['sample_id'])
-        run(
-            input_path=row['fq_path'], output_path=result_dir, input_class='one',
-            file_class='fq', debug=debug, sample_id=row['sample_id'], np=10,include_x=include_x,ref=ref
-        )
-
-@app.command()
-def run_file_parallel(
-    workers: int = typer.Option(help='使用CPU核数量', default=4),
-    sample_info: str = typer.Option(..., help='样本信息表格, TAB分割，"sample_id fq-path"'),
-    result_dir: str = typer.Option(..., help='结果文件夹'),
-    debug: bool = typer.Option(help='输出debug信息', default=False),
-    ref: str = typer.Option(help='基因组版本号', default='hg19'),
-    include_x:bool = typer.Option(help='是否输出X染色体信息', default=False),
-):
-    debug_flag = '--debug' if debug else '--no-debug'
-    include_x = '--include-x' if include_x else '--no-include-x'
-    sample_df = pd.read_csv(sample_info, sep='\t', names=['sample_id', 'fq_path'])
-    chunk_size = int(len(sample_df) / workers)
-    task_li = []
-    with tempfile.TemporaryDirectory(dir=result_dir) as tmp_dir:
-        for idx, chunk in enumerate([sample_df[i:i+chunk_size] for i in range(0, len(sample_df), chunk_size)]):
-            tmp_sample_info = os.path.join(tmp_dir, f'{idx}.tsv')
-            chunk.to_csv(tmp_sample_info, sep='\t', index=False, header=False)
-            task_li.append(
-                sub.Popen([
-                    "python", "-m", "heteroploidy_hunter", "run-file", "--sample-info",
-                    tmp_sample_info, "--result-dir", result_dir,"--ref", ref, debug_flag,include_x
-                    ])
-                )
-        for task in task_li:
-            task.wait()
-    print('finish all tasks')
-
 
 @app.command()
 def rnn_pre_loh(
     input_pkl: str = typer.Argument(help='pkl result file', default=None),
     output_dir: str = typer.Argument(help='output directory', default=None),
-    thread_num: int = typer.Option(help='使用CPU核数量', default=22),
-    interval: int = typer.Option(help='合并区域的间隔大小', default=2100000),
-    length: int = typer.Option(help='筛选LOH的区域长度', default=3500000),
-    deepth: bool = typer.Option(help='深度是否大于0.5', default=True),
-    include_x:bool = typer.Option(help='是否输出X染色体信息', default=False)
+    thread_num: int = typer.Option(help='Number of CPU cores used', default=22),
+    interval: int = typer.Option(help='The interval size of the merge area', default=2100000),
+    length: int = typer.Option(help='Filter the area length of the LOH', default=3500000),
+    deepth: bool = typer.Option(help='The depth is greater than 0.5X,used to select a different model, the default is 125KB model', default=True),
+    include_x:bool = typer.Option(help='Whether to output X chromosome information', default=False)
 ):
     pre_loh.run_loh_pre(input_pkl,output_dir,thread_num,interval,length,deepth,include_x=include_x)
 
